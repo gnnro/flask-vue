e5a70931afe407ffc4cdfd28ec879caa55b04a52
Gennaro <g.anzalone12.com>
2018-10-18 15:17:06 -0700

    add table for movies with buttons

--- client/src/components/Movies.vue
+++ client/src/components/Movies.vue
@@ -1,6 +1,36 @@
 <template>
     <div class="container">
-      <p>movies</p>
+      <div class="row">
+        <div class="col-sm-10">
+          <h1>Movies</h1>
+          <hr><br><br>
+          <button type="button" class="btn btn-success btn-sm">Add Movie</button>
+          <br><br>
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Director</th>
+                <th scope="col">Starring</th>
+                <th scope="col">Watched?</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+             <tr>
+               <td>foo</td>
+               <td>bar</td>
+               <td>baz</td>
+               <td>foobarbaz</td>
+               <td>
+                 <button type="button" class="btn btn-warning btn-sm">Update</button>
+                 <button type="button" class="btn btn-danger btn-sm">Delete</button>
+               </td>
+             </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
 </template>
 
