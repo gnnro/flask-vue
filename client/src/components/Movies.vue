4aba0248dd7d1287f2dded2d74cd87c6da6584de
Gennaro <g.anzalone12.com>
2018-10-18 15:06:26 -0700

    create movies component

---
+++ client/src/components/Movies.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+      <p>movies</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Movies"
+    }
+</script>
+
+<style scoped>
+
+</style>
