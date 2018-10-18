3a5294f2f40dd6c3ef44dcf271a830571f40bcd7
Gennaro <g.anzalone12.com>
2018-10-18 15:53:13 -0700

    add movies list and js call

--- server/app.py
+++ server/app.py
@@ -12,11 +12,35 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app)
 
+MOVIES = [
+    {
+        'title': 'Saving Private Ryan',
+        'director': 'Steven Spielberg',
+        'starring': 'Tom Hanks, Matt Damon',
+        'watched': True
+    },
+    {
+        'title': 'Goodfellas',
+        'director': 'Martin Scorsese',
+        'starring': 'Ray Liotta, Robert DeNiro, Joe Pesci',
+        'watched': True
+    },
+    {
+        'title': 'Avengers: Infinity War',
+        'director': 'Anthony Russo',
+        'starring': 'Robert Downey Jr, Chris Hemsworth',
+        'watched': False
+    }
+]
+
 
 # check route
-@app.route('/ping', methods=['GET'])
-def ping_pong():
-    return jsonify('pong!')
+@app.route('/movies', methods=['GET'])
+def all_movies():
+    return jsonify({
+        'status': 'success',
+        'movies': MOVIES
+    })
 
 
 if __name__ == '__main__':
