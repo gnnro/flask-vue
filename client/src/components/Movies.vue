797df5f97f384b595319dde2ee0e742a8f3091cd
Gennaro <g.anzalone12.com>
2018-10-19 14:33:42 -0700

    add edit movie modal

--- client/src/components/Movies.vue
+++ client/src/components/Movies.vue
@@ -86,6 +86,50 @@
           <b-button type="reset" variant="danger">Reset</b-button>
         </b-form>
       </b-modal>
+      <b-modal ref="editMovieModal"
+               id="movie-update-modal"
+               title="Update"
+               hide-footer>
+        <b-form @submit="onSubmitUpdate" @reset="onResetUpdate" class="w-100">
+          <b-form-group id="form-title-edit-group"
+                        label="Title:"
+                        label-for="form-title-edit-input">
+            <b-form-input id="form-title-edit-input"
+                          type="text"
+                          v-model="editForm.title"
+                          required
+                          placeholder="Enter title">
+            </b-form-input>
+          </b-form-group>
+          <b-form-group id="form-director-edit-group"
+                        label="Director:"
+                        label-for="form-director-edit-input">
+            <b-form-input id="form-director-edit-input"
+                          type="text"
+                          v-model="editForm.director"
+                          required
+                          placeholder="Enter director">
+            </b-form-input>
+          </b-form-group>
+          <b-form-group id="form-starring-edit-group"
+                        label="Starring:"
+                        label-for="form-starring-edit-input">
+            <b-form-input id="form-starring-edit-input"
+                          type="text"
+                          v-model="editForm.starring"
+                          required
+                          placeholder="Enter actors">
+            </b-form-input>
+          </b-form-group>
+          <b-form-group id="form-watched-edit-group">
+            <b-form-checkbox-group v-model="editForm.watched" id="form-checks">
+              <b-form-checkbox value="true">Watched?</b-form-checkbox>
+            </b-form-checkbox-group>
+          </b-form-group>
+          <b-button type="submit" variant="primary">Update</b-button>
+          <b-button type="reset" variant="danger">Cancel</b-button>
+        </b-form>
+      </b-modal>
     </div>
 </template>
 
